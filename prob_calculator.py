82205deddd4cd2a9930e7fbb79aab8009d28cf28
diogoguimaraesr <diogo.m.guimaraes.92@gmail.com>
2021-01-18 14:01:28 +0000

    build contents variable

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,5 +4,19 @@ import random
 
 class Hat:
 
+  def __init__(self, **kwargs):
+    self.contents = []
+    self.ball_dist = {}
+
+    for i, color in enumerate(list(kwargs.keys())):
+      self.add_balls_to_list(color, list(kwargs.values())[i])
+
+    for color in self.ball_dist.keys():
+      for i in range(self.ball_dist[color]):
+        self.contents.append(color)
+
+  def add_balls_to_list(self, color, amount):
+    self.ball_dist[color] = amount
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  pass
